f884e484d40f50bc6bdd3a9ab29075e277c24450
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-06 19:22:12 +0600

    added testimonial model

--- src/modules/User/user.interface.ts
+++ src/modules/User/user.interface.ts
@@ -1,6 +1,16 @@
+import { ObjectId } from 'mongoose';
+
 export interface IUser {
   fullName: string;
   email: string;
   role: 'student' | 'tutor' | 'admin';
   avatar: string;
+}
+
+export interface ITestimonial {
+  createdBy: ObjectId;
+  userName: string;
+  avatar: string;
+  message: string;
+  rating: number;
 }
