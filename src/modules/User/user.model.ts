59e735fc29233d51c19fd758a52f229d9873d49b
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-06 19:22:12 +0600

    added testimonial model

--- src/modules/User/user.model.ts
+++ src/modules/User/user.model.ts
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose';
-import { IUser } from './user.interface';
+import { ITestimonial, IUser } from './user.interface';
 
+// define the User schema
 const userSchema = new Schema<IUser>({
   fullName: {
     type: String,
@@ -10,8 +11,8 @@ const userSchema = new Schema<IUser>({
     type: String,
     required: true,
     unique: true,
-    lowercase: true,
-    trim: true,
+    lowercase: true, // convert email to lowercase
+    trim: true, // trim whitespace from email
   },
   avatar: {
     type: String,
@@ -19,8 +20,8 @@ const userSchema = new Schema<IUser>({
   },
   role: {
     type: String,
-    enum: ['student', 'tutor', 'admin'],
-    default: 'student',
+    enum: ['student', 'tutor', 'admin'], // only allow specific roles
+    default: 'student', // default role is 'student'
   },
 });
 
@@ -31,5 +32,38 @@ userSchema.methods.toJSON = function () {
   return userObject;
 };
 
-// create the user model using the schema
+// create the User model using the schema
 export const User = model<IUser>('User', userSchema);
+
+// define the Testimonial schema
+const testimonialSchema = new Schema<ITestimonial>({
+  createdBy: {
+    type: Schema.Types.ObjectId, // reference to a User
+    required: true,
+    ref: 'User', // reference to the User model
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1, // minimum value for rating
+    max: 5, // maximum value for rating
+  },
+});
+
+// create the Testimonial model using the schema
+export const Testimonial = model<ITestimonial>(
+  'Testimonial',
+  testimonialSchema,
+);
