86eca89ca691509185215854d4a700d72e3573df
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-06 15:39:15 +0600

    update middlewares

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -1,11 +1,8 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
-const notFound = (req: Request, res: Response, next: NextFunction) => {
-  return res.status(httpStatus.NOT_FOUND).json({
+const notFound = (req: Request, res: Response) => {
+  return res.status(httpStatus?.NOT_FOUND).json({
     success: false,
     message: 'Api Not Found!',
   });
