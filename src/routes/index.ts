8aa309bcf012cf0f5272174b5a05e043c5a5a716
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-06 22:06:06 +0600

    remove husky and lint staged

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,10 @@ import { UserRoutes } from '../modules/User/user.route';
 
 const router = Router();
 
-const routes = [{ path: '/users', route: UserRoutes }];
+const routes = [
+  { path: '/users', route: UserRoutes },
+  { path: '/tutors', route: tutorrou },
+];
 
 routes?.forEach((route) => router?.use(route?.path, route?.route));
 
