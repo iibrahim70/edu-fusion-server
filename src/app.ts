7af06e327de74e1556ba2511eb6ab158488b410c
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-07 14:35:47 +0600

    added cors configuaration

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,56 @@
+import cors, { CorsOptions } from 'cors';
 import express, { Request, Response } from 'express';
-import cors from 'cors';
 import router from './routes';
 import globalErrorHandler from './middlewares/globalErrorHandler';
 import notFound from './middlewares/notFound';
 
 const app = express();
 
-// middlewares
-app.use(express.json());
-app.use(cors());
+// whitelist of allowed origins for CORS
+const whitelist = ['http://localhost:5173', 'https://edu-fusion.netlify.app'];
 
-// default route
+// CORS options to allow requests only from whitelisted origins
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist?.indexOf(origin as string) !== -1 || !origin) {
+      callback(null, true); // Allow request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Deny request
+    }
+  },
+};
+
+// Middlewares
+app.use(express.json()); // for parse JSON bodies in incoming requests
+app.use(cors(corsOptions)); // for enable CORS with specified options
+
+// Default route for the root URL
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  const serverStatus = {
+    status: 'running',
+    message: 'Edu Fusion API is operational and running smoothly.',
+    timestamp: new Date().toISOString(),
+    version: 'v1.0.1',
+    uptime: process.uptime(),
+    author: {
+      name: 'Ibrahim Khalil',
+      email: 'iibrahiim.dev@gmail.com',
+      website: 'https://iibrahim-dev.netlify.app/',
+    },
+    contact: {
+      support: 'iibrahiim.dev@gmail.com',
+      website: 'https://edu-fusion.netlify.app/',
+    },
+  };
+
+  res.json(serverStatus);
 });
 
-// application routes
+// Application routes under the '/api/v1' path
 app.use('/api/v1', router);
 
-// global error handler
-app.use(globalErrorHandler);
-
-// not found route
-app.use(notFound);
+// Error-handling Middlewares
+app.use(globalErrorHandler); // Global error handler middleware
+app.use(notFound); // Middleware to handle 404 - Not Found errors
 
 export default app;
