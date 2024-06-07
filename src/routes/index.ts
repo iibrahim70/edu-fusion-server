7e5c5512b57d9466fa0cd83416a77b1139165be2
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-07 12:37:27 +0600

    update main routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,22 @@
 import { Router } from 'express';
-import { UserRoutes } from '../modules/User/user.route';
+import { CommentRoutes } from '../modules/Comments/comment.route';
 import { CourseRoutes } from '../modules/Course/course.route';
-import { TestimonialRoutes } from '../modules/Testimonial/testimonial.route';
 import { ModuleRoutes } from '../modules/Module/module.route';
+import { RatingRoutes } from '../modules/Rating/rating.route';
+import { SubModuleRoutes } from '../modules/SubModule/subModule.route';
+import { TestimonialRoutes } from '../modules/Testimonial/testimonial.route';
+import { UserRoutes } from '../modules/User/user.route';
 
 const router = Router();
 
 const routes = [
-  { path: '/users', route: UserRoutes },
+  { path: '/comments', route: CommentRoutes },
   { path: '/courses', route: CourseRoutes },
   { path: '/modules', route: ModuleRoutes },
+  { path: '/ratings', route: RatingRoutes },
+  { path: '/sub-modules', route: SubModuleRoutes },
   { path: '/testimonials', route: TestimonialRoutes },
+  { path: '/users', route: UserRoutes },
 ];
 
 routes?.forEach((route) => router?.use(route?.path, route?.route));
