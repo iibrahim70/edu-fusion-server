43f06f514d416cd6aa1a9cc653e836873816428c
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-08 16:03:15 +0600

    user module added role update api

--- src/modules/User/user.controller.ts
+++ src/modules/User/user.controller.ts
@@ -7,20 +7,9 @@ const createUser = catchAsync(async (req, res) => {
   const result = await UserServices?.createUserIntoDB(req?.body);
 
   sendResponse(res, {
-    statusCode: httpStatus?.CREATED,
+    statusCode: httpStatus.CREATED,
     success: true,
     message: 'User created successfully!',
-    data: result,
-  });
-});
-
-const getUsers = catchAsync(async (req, res) => {
-  const result = await UserServices?.getUsersFromDB(req?.query);
-
-  sendResponse(res, {
-    statusCode: httpStatus?.OK,
-    success: true,
-    message: 'User list retrieved successfully!',
     data: result,
   });
 });
@@ -31,15 +20,41 @@ const createJwtToken = catchAsync(async (req, res) => {
   const result = await UserServices?.createJwtTokenIntoDB(email);
 
   sendResponse(res, {
-    statusCode: httpStatus?.CREATED,
+    statusCode: httpStatus.CREATED,
     success: true,
     message: 'Token created successfully!',
     data: result,
   });
 });
 
+const getUsers = catchAsync(async (req, res) => {
+  const result = await UserServices?.getUsersFromDB(req?.query);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User list retrieved successfully!',
+    data: result,
+  });
+});
+
+const updateUserRole = catchAsync(async (req, res) => {
+  const result = await UserServices?.updateUserRoleFromDB(
+    req?.params?.userId,
+    req?.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User role updated successfully!',
+    data: result,
+  });
+});
+
 export const UserControllers = {
   createUser,
-  getUsers,
   createJwtToken,
+  getUsers,
+  updateUserRole,
 };
