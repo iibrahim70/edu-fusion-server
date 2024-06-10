ada17c42696119fc4eceedf3ed73779e23c8610a
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-10 13:23:44 +0600

    :recycle: update testimonial module

--- src/modules/Testimonial/testimonial.route.ts
+++ src/modules/Testimonial/testimonial.route.ts
@@ -3,7 +3,7 @@ import { TestimonialControllers } from './testimonial.controller';
 
 const router = Router();
 
-router.get('/', TestimonialControllers?.getTestimonials);
-router.post('/create-testimonial', TestimonialControllers?.createTestimonial);
+router.get('/', TestimonialControllers.getTestimonials);
+router.post('/create-testimonial', TestimonialControllers.createTestimonial);
 
 export const TestimonialRoutes = router;
